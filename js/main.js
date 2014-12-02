b4717eb26ccc0c9bea21bcf7261b83467e263bac
Will <bigredwill@gmail.com>
2014-12-02 15:55:44 -0800

    better image handling

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
 var WhatColor =  (function () {
-    var imageUrls = [],
+    var imageUrls,
       images = [],
+      colors = [],
       CF = new ColorThief();
 
-    var grabImagesFromUrls = function (urls) {
-      var i;
-      for (i = 0; i < urls.length; i += 1) {
-        images.push(getImageFromUrl(urls[i].url));
 
-      }
-    };
-    //Gets an Image from an url
-    var getImageFromUrl = function (url) {
-      return $.ajax({
-        url: url,
-        datatype: "image/jpg",
-        crossDomain: true,
-        success: function (data) {
-          console.log(data);
-        },
-        error: function (data) {
-          console.log(data);
+    // var camColorSteal = function() {
+    //   CF.getPalette(images[0], 1);
+    // };
+
+    var loadCams = function() {
+      images = [];
+      var grabImagesFromUrls = function (urls) {
+        var i;
+        for (i = 0; i < urls.length; i += 1) {
+          images.push(getImageFromUrl(urls[i].url));
         }
-      });
+      };
+      //Gets an Image from an url
+      var getImageFromUrl = function (url) {
+        var img = document.createElement('img');
+        img.src = url;
+        return img;
+      };
+      if(!imageUrls){
+        $.ajax({
+         url: "./assets/cams.json",
+          complete: function (data) {
+            imageUrls = data.responseJSON.cams;
+            grabImagesFromUrls(imageUrls);
+          }
+        });
+      }
+      
     };
 
-    $.ajax({
-      url: "./assets/cams.json",
-      complete: function (data) {
-        imageUrls = data.responseJSON.cams;
-        grabImagesFromUrls(imageUrls);
-      }
-    });
+    loadCams();
+    // camColorSteal();
 
   }());
