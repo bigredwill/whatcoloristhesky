87c29706b540b94799fde676625a8c33ac40a3e1
Will <bigredwill@gmail.com>
2014-12-02 15:16:31 -0800

    ajaxy

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var WhatColor =  (function () {
+    var imageUrls = [],
+      images = [],
+      CF = new ColorThief();
+
+    var grabImagesFromUrls = function (urls) {
+      var i;
+      for (i = 0; i < urls.length; i += 1) {
+        images.push(getImageFromUrl(urls[i].url));
+
+      }
+    };
+    //Gets an Image from an url
+    var getImageFromUrl = function (url) {
+      return $.ajax({
+        url: url,
+        crossDomain: true,
+        complete: function (data) {
+          console.log(data);
+        }
+      });
+    };
+
+    $.ajax({
+      url: "/assets/cams.json",
+      complete: function (data) {
+        imageUrls = data.responseJSON.cams;
+        grabImagesFromUrls(imageUrls);
+      }
+    });
+
+  }());
